61878f1098e6e37a39a4aa61325ac92767d1deb6
RickKtips <rick.hades@gmail.com>
2026-06-17 15:44:18 -0300

    Estrutura inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { loadRemoteModule } from '@angular-architects/module-federation';
+const MICROFRONTEND_URL = 'http://localhost:4202';
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./features/home/home').then(m => m.Home) },
+  // {
+  // path: 'micro1',
+  //   loadComponent: () =>
+  //     loadRemoteModule({
+  //       remoteEntry: 'http://localhost:4202/remoteEntry.js',
+  //       exposedModule: './Component',
+  //       type: 'module',
+  //     }).then((m) => m.App),
+  // },
+  {
+    path: 'pokemons',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: `${MICROFRONTEND_URL}/remoteEntry.js`,
+        exposedModule: './Routes',
+      }).then((m) => m.routes),
+  },
+];
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'hostMicrofront',
+  remotes: {
+    pokemons: 'pokemons@http://localhost:4202/remoteEntry.js',
+  },
+  exposes: {
+    './Component': './src/app/app.ts',
+  },
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+  
+});
